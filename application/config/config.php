18764c8a671a39c05648430f0afe8db2528f2687
Elektropay <richard.r@everpayinc.com>
2019-10-20 17:41:45 -0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://elektropay-app.herokuapp.com/';
+$config['base_url'] = 'https://everpaygateway-og.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
